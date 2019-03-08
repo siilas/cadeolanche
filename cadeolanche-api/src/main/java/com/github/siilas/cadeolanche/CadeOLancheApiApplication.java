a97df1c286dd44d056225129031f3931a2b428d5
Silas Maciel Ferreira <s2it_sferreira@uolinc.com>
2019-03-08 13:12:15 -0300

    Initial commit

---
+++ cadeolanche-api/src/main/java/com/github/siilas/cadeolanche/CadeOLancheApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.siilas.cadeolanche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadeOLancheApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadeOLancheApiApplication.class, args);
+    }
+
+}
---
+++ cadeolanche-web/src/main/java/com/github/siilas/cadeolanche/CadeOLancheWebApplication.java
@@ -0,0 +1,13 @@
+package com.github.siilas.cadeolanche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadeOLancheWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadeOLancheWebApplication.class, args);
+    }
+
+}
